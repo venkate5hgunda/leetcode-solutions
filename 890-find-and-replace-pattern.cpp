bfa98a160ac0c5b3df2b1e960a1e41142a4f4761
Venkatesh Gunda <venkateshgunda1997@gmail.com>
2021-05-21 23:04:13 +0530

    Update 890-find-and-replace-pattern.cpp

--- 890-find-and-replace-pattern.cpp
+++ 890-find-and-replace-pattern.cpp
@@ -24,3 +24,31 @@ public:
 };
 
 
+// LEETCODE ANSWER APPROACH. CREATE A CIPHER WITH THE GIVEN PATTERN, AND TRANSLATE ALL THE STRINGS AND THEN COMPARE. SAVES ON SPACE. O(N2) TIME, O(1) SPACE
+class Solution {
+public:
+    vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
+        vector<string> matches;
+        cipher=pattern;
+        for(int i=0;i<pattern.size();i++) {
+            cipher[i]=translate(cipher[i]);
+        }
+        for(int i=0;i<words.size();i++) {
+            compareAndAdd(words[i],matches);
+        }
+        return matches;
+    }
+    map<char,char> codex;
+    string cipher;
+    char translate(char c) {
+        if(codex.find(c)==codex.end()) codex[c]=(char)('a'+codex.size());
+        return codex[c];
+    }
+    void compareAndAdd(string word, vector<string>& matches) {
+        codex.clear();
+        for(int i=0;i<word.size();i++) {
+            if(translate(word[i])!=cipher[i]) return;
+        }
+        matches.push_back(word);
+    }
+};
